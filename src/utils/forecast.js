3cd844986e084c9b30ebc1907fa0e6b562c2ea5b
Ashish Dwivedi <Ashish.Dwivedi@akelius.de>
2019-08-25 16:55:07 +0200

    Added more forecast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,11 @@ const forecast = (longitude, lattitude, callback) => {
         } else if (body.error) {
             callback('Invalid location. Please try again');
         } else {
+            console.log(body.daily.data[0]);
             callback(undefined, {
-                summary: `${ body.daily.data[0].summary } It is currently ${ body.currently.temperature } degrees out. There is a ${ body.currently.precipProbability } % chance of rain.`,
+                summary: `${ body.daily.data[0].summary } It is currently ${ body.currently.temperature } degrees out.
+                    There is a ${ body.currently.precipProbability } % chance of rain. Maximum temperature will be
+                    ${ body.daily.data[0].temperatureMax } and the minimum will be ${ body.daily.data[0].temperatureMin }`,
                 temperature: body.currently.temperature,
                 precipProbability: body.currently.precipProbability
             });
